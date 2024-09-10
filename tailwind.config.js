a7d0bf34d6028de5ac20c1cbddc76d4e8bb37946
Taha Aaqib <m.tahaaaqib@gmail.com>
2024-09-11 00:59:25 +0500

    Remaing files.

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/*.html',   // Include your HTML files
+    './src/**/*.{html,js}',],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    // container: {
+    //   center: true,
+    //   padding: '2rem'
+    // },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12,88%,59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+      spacing: {
+        '38': '9.5rem',  // Adding 9.5rem as custom spacing
+      }
+    },
+  },
+  plugins: [],
+}
+
